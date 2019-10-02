4d79cd6ad01bea7e61518656247050bbcde5e639
adam casey <adam-casey@live.com>
2019-10-02 12:54:36 -0600

    Create python_ref.py

---
+++ python_ref.py
@@ -0,0 +1,10 @@
+def find_pairs(values, target):
+	if target > 1:
+		value_dict = {}
+		for value in values:
+			if value in value_dictr:
+				value_dict[value] += 1
+			else:
+				value_dict[value] = 0
+				
+				

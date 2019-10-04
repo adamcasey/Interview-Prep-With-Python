96302c28e9aabb22f02310c1d848689782876557
adam casey <adam-casey@live.com>
2019-10-04 13:17:31 -0600

    add case_insensitive_compare()

--- python_ref.py
+++ python_ref.py
@@ -1,6 +1,36 @@
 '''
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 '''
+# Compare Two Strings (case insensitive, not optimal)
+# Time (always-case): O(n)
+# Space: O(n)
+def case_insensitive_compare(s1, s2):
+    if len(s1) != len(s2):
+        return False
+    s1_iter = iter(s1)
+    s2_iter = iter(s2)
+    # '_' means it's a variable meant to be discarded
+    for _ in range(len(s1)):
+        # grab the char on each string
+        s1_c = next(s1_iter)
+        #print(f's1_c: {s1_c}')
+        s2_c = next(s2_iter)
+        #print(f's1_c: {s2_c}')
+
+        if not s1_c.upper() == s2_c.upper():
+            return False
+        
+    return True
+
+print(case_insensitive_compare("adam", "ADAM"))
+print(case_insensitive_compare("adam", "Casey"))
+print(case_insensitive_compare("adam", ""))
+print(case_insensitive_compare("", ""))
+
+
+'''
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+'''
 # Longest Common Substring
 
 # Brute force solution:

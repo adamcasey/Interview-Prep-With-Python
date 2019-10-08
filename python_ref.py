3db06b42445fb24eb3626aac2935ec1e976bfac3
adam casey <adam-casey@live.com>
2019-10-07 18:13:06 -0600

    Add fibonacci methods

--- python_ref.py
+++ python_ref.py
@@ -2,7 +2,65 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 '''
 # Find the k'th largest element in a list
+import random
 
+# The 1st largest element is the largest element
+# The kth largest element is n - k after the array is logically sorted
+def findKthLargest(arr, k):
+	
+  # Keep track of the 'left' and 'right' space in which the k'th largest element
+  # can possibly be, we will use these bounds to know what section to actually partition \
+  # around a chosen pivot.
+  n = len(arr)
+  left = 0
+  right = n - 1
+
+  while left <= right:
+    # Pick a random pivot --> except for the final index position the first time the loop is run
+    choosenPivotIndex = random.randint(left , right)
+    #print("choosenPivotIndex: {}".format(choosenPivotIndex))
+    # Execute the actual partitioning and get back the final position \
+    # of the pivot we choose after partitioning is over
+    finalIndexOfChoosePivot = partition(arr, left, right, choosenPivotIndex)
+    
+    if (finalIndexOfChoosePivot == n - k):
+      return arr[finalIndexOfChoosePivot]
+    elif (finalIndexOfChoosePivot > n - k):
+      right = finalIndexOfChoosePivot - 1
+    else:
+      left = finalIndexOfChoosePivot + 1
+
+  return - 1
+
+def partition(arr, left, right, pivotIndex):
+  pivotValue = arr[pivotIndex]
+  #print("pivotValue: {}".format(pivotValue))
+  lesserItemsTailIndex = left
+  #print("lesserItemsTailIndex: {}".format(lesserItemsTailIndex))
+  swap_help(arr, pivotIndex, right)
+
+  for i in range(left, right, 1):
+    if arr[i] < pivotValue:
+      swap_help(arr, i, lesserItemsTailIndex)
+      lesserItemsTailIndex += 1
+
+  swap_help(arr, right, lesserItemsTailIndex)
+
+  return lesserItemsTailIndex
+
+def swap_help(arr, first, second):
+  temp = arr[first]
+  arr[first] = arr[second]
+  arr[second] = temp
+
+  #print("arr: {}".format(arr))
+
+temp_list = [3, 2, 1, 5, 6, 4]
+k = 2
+
+# for _ in range(0, 100000):
+#   if findKthLargest(temp_list, k) != 5:
+#     print("failed somewhere")
 
 '''
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -160,6 +218,37 @@ print(longest_common_substring("AdamAdamAdam", "MadamMadamMadam"))
 '''
 
 # Fibonacci Numbers
+
+def fibonacci(n):
+  if n == 1 or n == 2: return 1
+  return fibonacci(n - 1) + fibonacci(n - 2)
+
+def fibonacci_memoization(n):
+  fibo = dict()
+  def rec(n):
+    if n in fibo: return fibo[n]
+
+    if n == 1 or n == 2:
+      fibo[n] = 1
+    else:
+      fibo[n] = rec(n - 1) + rec(n - 2)
+    return fibo[n]
+  return rec(n)
+
+def fibonacci_bottom_up(n):
+  if n == 1 or n == 2: return 1
+
+  fib0 = 1
+  fib1 = 1
+  for i in range(3, n+1):
+    new_fib = fib0 + fib1
+    fib0 = fib1
+    fib1 = new_fib
+  return fib1
+
+n = 10
+print(fibonacci_memoization(n))
+print(fibonacci_bottom_up(n))
 
 '''
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
